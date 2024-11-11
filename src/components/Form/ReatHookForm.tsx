fd2f45215dc8f0fb1cec32e0c2f0f4477a4402c6
Demis Getachew <demisgech@gmail.com>
2024-11-11 14:58:18 +0300

    Apply form validation using react-hook-form

--- src/components/Form/ReatHookForm.tsx
+++ src/components/Form/ReatHookForm.tsx
@@ -1,7 +1,15 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  email: string;
+}
 const ReactHookForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -12,22 +20,31 @@ const ReactHookForm = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">
           Email
         </label>
         <input
-          {...register("email")}
+          {...register("email", { required: true })}
           type="email"
           className="form-control"
           id="email"
         />
+        {errors.email?.type === "required" && (
+          <p>The email field is required</p>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Register
