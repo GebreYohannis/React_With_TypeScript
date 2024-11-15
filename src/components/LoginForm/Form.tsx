646094976a91a71a7f11389e87ed9e07850a3076
Demis Getachew <demisgech@gmail.com>
2024-11-15 08:54:02 +0300

    Remove unnecessary div

--- src/components/LoginForm/Form.tsx
+++ src/components/LoginForm/Form.tsx
@@ -73,15 +73,14 @@ const Form = () => {
           Remember me
         </label>
       </div>
+
       <div className="mb-3">
-        <div className="mb-3">
-          <button className="btn btn-primary" type="submit">
-            Log in
-          </button>
-          <button type="reset" className="btn btn-secondary m-4">
-            Clear
-          </button>
-        </div>
+        <button className="btn btn-primary" type="submit">
+          Log in
+        </button>
+        <button type="reset" className="btn btn-secondary m-4">
+          Clear
+        </button>
       </div>
     </form>
   );
