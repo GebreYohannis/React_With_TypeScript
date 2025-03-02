9fa5ee277547231ef064c00c9c736cda8ef3d8de
Demis Getachew <demisgech@gmail.com>
2025-03-02 15:16:09 +0300

    Refactor: use react query to fetch albums

--- src/react-query/hooks/useAlbum.ts
+++ src/react-query/hooks/useAlbum.ts
@@ -1,33 +1,19 @@
-import axios, { AxiosError, CanceledError } from "axios";
-import { useEffect, useState } from "react";
-
-interface Album {
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+export interface Album {
   id: number;
   title: string;
   userId: number;
 }
 
 const useAlbum = () => {
-  const [albums, setAlbums] = useState<Album[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get<Album[]>("https://jsonplaceholder.typicode.com/albums")
-      .then((response) => {
-        setLoading(false);
-        setAlbums(response.data);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError((error as AxiosError).message);
-        setLoading(false);
-      });
-  }, []);
-
-  return { albums, error, isLoading };
+  return useQuery<Album[], Error>({
+    queryKey: ["albums"],
+    queryFn: () =>
+      axios
+        .get<Album[]>("https://jsonplaceholder.typicode.com/albums")
+        .then((response) => response.data),
+  });
 };
 
 export default useAlbum;
