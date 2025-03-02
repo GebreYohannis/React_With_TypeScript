ceb5441d44c20b98a495720af7fbd1cf3154750f
Demis Getachew <demisgech@gmail.com>
2025-03-02 13:45:24 +0300

    Show loader

--- src/react-query/AlbumList.tsx
+++ src/react-query/AlbumList.tsx
@@ -10,21 +10,27 @@ interface Album {
 const AlbumList = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get<Album[]>("https://jsonplaceholder.typicode.com/albums")
       .then((response) => {
-        // console.log(response.data);
+        setLoading(false);
         setAlbums(response.data);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError((error as AxiosError).message);
+        setLoading(false);
       });
   }, []);
 
+  if (isLoading) return <p className="spinner-border"></p>;
+
   if (error) return <p className="alert alert-danger">{error}</p>;
+
   return (
     <ul className="list-group">
       {albums.map((album) => (
