83cbf33c176227314f2bac00d594f4b9f744224c
Demis Getachew <demisgech@gmail.com>
2025-03-06 19:16:55 +0300

    Data about the current route

--- src/routing/UserDetailsPage.tsx
+++ src/routing/UserDetailsPage.tsx
@@ -1,4 +1,20 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 function UserDetailsPage() {
+  const params = useParams();
+  console.log(params);
+
+  // The following hook has side effert so be carefull when using it
+  // You should only call it inside:
+  // 1. Event handler
+  // 2. useEffect hook
+  const [searchParams, setSearchParams] = useSearchParams();
+  //   console.log(searchParams.toString());
+  const name = searchParams.get("name");
+  console.log("name: " + name);
+  setSearchParams();
+  const location = useLocation();
+  console.log(location);
   return <div>User</div>;
 }
 
